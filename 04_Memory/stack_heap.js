e7e04e8c4289e6a4b663f40293a4c8b7461719a0
Pankaj-Ksh <121474063+Pankaj-Ksh@users.noreply.github.com>
2024-04-20 18:22:47 +0000

    Stack And Heap Memory

---
+++ 04_Memory/stack_heap.js
@@ -0,0 +1,8 @@
+/*
+    stack Memory : for the Primitive DataTypes value ( Copy of variable is given )
+                   There are Seven Datatypes (String, Number, Boolean, undefined, Bigint, Symbol , null)
+
+                   
+    heap Memory : for the Non-Primitive DataTypes value ( Reference of variable is given )
+                  There are Three Datatypes (Array, Objects, Functions)
+*/
