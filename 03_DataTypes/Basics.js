cd41c7379c45a322237df24b64066ed48ea9e466
Pankaj-Ksh <121474063+Pankaj-Ksh@users.noreply.github.com>
2024-04-20 04:12:10 +0000

    Things Related to DataType

---
+++ 03_DataTypes/Basics.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all JS code as newer version of JS
+
+// alert(3 + 3); // we are using nodejs, not browser , it will give error 
+
+
+let name = "Pankaj"
+let age = 20;
+let height = 20.2;
+let isLoggedIn = true
+let Password = null;
+let state;
+
+
+// In JavaScript, a primitive (primitive value, primitive data type) is data that is not an object and has no methods or properties. 
+//There are 7 primitive data types:
+
+// 1. number => range from 2 to power 53 , JavaScript has only one type of number. Numbers can be written with or without decimals.
+// 2. bigint => for bigger value
+// 3. string => ""
+// 4. boolean => true/false
+// 5. null => standalone value,  In JavaScript, null is marked as one of the primitive values. However, when using the typeof operator, it returns "object".
+// 6. undefined => 
+// 7. symbol => unique
+
+
+console.log("Type of undefined is : ", typeof undefined); // undefined
+console.log("Type of Null is : ", typeof null); // object
