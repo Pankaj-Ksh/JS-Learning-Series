200d1ac6b775ec065f6034092379d3e5b7f2d6fb
Pankaj-Ksh <121474063+Pankaj-Ksh@users.noreply.github.com>
2024-04-27 18:01:28 +0000

    map()

---
+++ 16_Iterations/map_function.js
@@ -0,0 +1,6 @@
+// Map() :- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNumers.map( (num) => { return num + 10})
+
